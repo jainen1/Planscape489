5e2a5fb49158d384371df3cfb2d89375f869afcb
jainen1 <jainen@kaufmannfamily.com>
2026-06-13 13:27:01 -0700

    Renamed MenuManager to ManagerSummoner
    In honor of its necessity and power.

--- Assets/Scripts/ManagerSummoner.cs
+++ Assets/Scripts/ManagerSummoner.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class MenuManager : MonoBehaviour
+public class ManagerSummoner : MonoBehaviour
 {
     void Start() { GlobalGameManager.SendThemeUpdate(); } //removing this single line of code prevents the theme system AND GlobalGameManager from loading at the start of a game. I have tried to migrate it. I have failed.
 }
